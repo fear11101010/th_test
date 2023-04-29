0647f01a5a6c0155c90bd6aed216b981fd6dd983
shuvo <arafat.hossain>
2023-04-30 01:06:27 +0600

    first commit

---
+++ src/test/java/com/example/springbootmvctest/SpringBootMvcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmvctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
